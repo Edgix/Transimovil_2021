2a3c6e18eb4eb2f7e1499ac1443430c47da931a1
alfonsogalguera <57268703+alfonsogalguera@users.noreply.github.com>
2021-11-29 19:28:53 -0600

    señalamientos
    señalamientos

--- screens/Señalamientos.js
+++ screens/Señalamientos.js
@@ -1,8 +1,30 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native-elements';
 
 export default function Señalamientos() {
     return (
-      <Text>Señalamientos</Text>
+<View style={{ top:40, flex:1, flexDirection:"column", alignItems:"center", }}>
+
+    <Image
+  source={ require('../assets/preventivas/curva.png')}
+  style={{width: 100, height: 100 }}
+/>
+      <Text>Preventiva</Text>
+       <Image
+  source={ require('../assets/restrictivas/no_rebasar.png')}
+  style={{width: 100, height: 100 }}
+/>
+      <Text>Restrictiva</Text>
+       <Image
+  source={ require('../assets/informativa/caseta.png')}
+  style={{ width: 100, height: 100 }}
+/>
+      <Text>Informativa</Text>
+
+
+
+
+    </View>
     );
   }
